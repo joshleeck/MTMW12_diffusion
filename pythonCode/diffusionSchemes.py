2e7f5cc2030a67a2e00e990e7b3521bf337e7512
joshleeck <32456936+joshleeck@users.noreply.github.com>
2017-10-17 21:30:06 +0800

    Updated diffusion and diffusionschemes

--- pythonCode/diffusionSchemes.py
+++ pythonCode/diffusionSchemes.py
@@ -6,3 +6,54 @@ import numpy as np
 # The linear algebra package for BTCS (for solving the matrix equation)
 import scipy.linalg as la
 
+def FTCS(phiOld, d, nt):
+    '''
+    Diffusion of profile in phiOld using FTCS using non-dimensional diffusion
+    coefficient, d
+    '''
+    
+    nx = len(phiOld)
+    
+    #new time-step array for phi
+    phi = phiOld.copy()
+    phi2
+    
+    #FTCS for all time steps
+    for it in xrange(int(nt)):
+        for x in xrange(1, nx-1):
+            phi[x+1] = phiOld[x] +  d* (phiOld[x+1] - 2*phiOld[x] + phiOld[x-1])
+            print((phiOld[x+1] - 2*phiOld[x] + phiOld[x-1]))
+        phiOld = phi.copy()
+        
+    return phi
+    
+def BTCS(phi, d, nt):    
+    '''
+    Diffusion of profile in phi using BTCS using non-dimensional diffusion 
+    coefficient, d assuming fixed value boundary conditions
+    '''
+
+    nx = len(phi)
+
+    #array representing BTCS
+    M = np.zeros([nx, nx])
+    
+    #zero gradient boundary conditions
+    M[0, 0] = 1
+    M[0, 1] = -1
+    M[-1, -1] = 1
+    M[-1, -2] = -1
+    for i in xrange(1, nx-1):
+        M[i, i-1] = -d
+        M[i, i] = 1+2*d
+        M[i, i+1] = -d
+        
+    #BTCS for all time steps
+    for it in xrange(int(nt)):
+        #RHS for zero gradient boundary conditions
+        phi[0] = 0
+        phi[-1] = 0
+        
+        phi=la.solve(M, phi)
+        
+    return phi
